7f077f2fb5ff951e748eac7bcdd9232ec475421c
christopheryecora <cey21b@fsu.edu>
2024-11-03 00:49:11 -0400

    implement PlaidLink and dwolla connections

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,28 +1,18 @@
 'use client';
-
+import { Button } from '@/components/ui/button';
+import { Form } from '@/components/ui/form';
+import { signIn, signUp } from '@/lib/actions/user.actions';
+import { authFormSchema } from '@/lib/utils';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Loader2 } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useState } from 'react';
-
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import { Button } from '@/components/ui/button';
-import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import CustomInput from './CustomInput';
-import { authFormSchema } from '@/lib/utils';
-import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import CustomInput from './CustomInput';
+import PlaidLink from './PlaidLink';
 
 const AuthForm = ({ type }: { type: string }) => {
     const router = useRouter();
@@ -48,7 +38,19 @@ const AuthForm = ({ type }: { type: string }) => {
             // Sign up with Appwrite & create plaid token
 
             if (type === 'sign-up') {
-                const newUser = await signUp(data);
+                const userData = {
+                    firstName: data.firstName!,
+                    lastName: data.lastName!,
+                    address1: data.address1!,
+                    city: data.city!,
+                    state: data.state!,
+                    postalCode: data.postalCode!,
+                    dateOfBirth: data.dateOfBirth!,
+                    ssn: data.ssn!,
+                    email: data.email,
+                    password: data.password,
+                };
+                const newUser = await signUp(userData);
 
                 setUser(newUser);
             }
@@ -58,8 +60,6 @@ const AuthForm = ({ type }: { type: string }) => {
                     email: data.email,
                     password: data.password,
                 });
-
-                console.log(response);
                 if (response) router.push('/');
             }
         } catch (error) {
@@ -103,7 +103,9 @@ const AuthForm = ({ type }: { type: string }) => {
                 </div>
             </header>
             {user ? (
-                <div className='flex flex-col gap-4'>{/* PlaidLink */}</div>
+                <div className='flex flex-col gap-4'>
+                    <PlaidLink user={user} variant='primary' />
+                </div>
             ) : (
                 <>
                     <Form {...form}>
